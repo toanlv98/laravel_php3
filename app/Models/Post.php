329a31ac59dfb3e9afa382260d5ad1cce9c76fd9
toanlv98 <toanlvph06512@fpt.edu.vn>
2019-09-26 20:04:09 +0700

    laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Post extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'content',
+    ];
+
+}
